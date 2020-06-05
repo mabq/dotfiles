9fb3ef64de0855f05241e6b2edc90a78f0b4f646
Manuel Aleandro Banderas Quirola <mabq@Manuels-MacBook-Pro.local>
2020-06-05 02:06:07 -0500

    brew.sh changes

--- brew.sh
+++ brew.sh
@@ -53,49 +53,75 @@ brew install sfnt2woff-zopfli
 brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+# brew install aircrack-ng
+# brew install bfg
+# brew install binutils
+# brew install binwalk
+# brew install cifer
+# brew install dex2jar
+# brew install dns2tcp
+# brew install fcrackzip
+# brew install foremost
+# brew install hashpump
+# brew install hydra
+# brew install john
+# brew install knock
+# brew install netpbm
+# brew install nmap
+# brew install pngcheck
+# brew install socat
+# brew install sqlmap
+# brew install tcpflow
+# brew install tcpreplay
+# brew install tcptrace
+# brew install ucspi-tcp # `tcpserver` etc.
+# brew install xpdf
+# brew install xz
 
 # Install other useful binaries.
-brew install ack
+# brew install ack
 #brew install exiv2
 brew install git
-brew install git-lfs
-brew install gs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rlwrap
-brew install ssh-copy-id
+# brew install git-lfs
+# brew install gs
+# brew install imagemagick --with-webp
+# brew install lua
+# brew install lynx
+# brew install p7zip
+# brew install pigz
+# brew install pv
+# brew install rename
+# brew install rlwrap
+# brew install ssh-copy-id
+brew install tldr
+brew install trash
 brew install tree
-brew install vbindiff
-brew install zopfli
+# brew install vbindiff
+# brew install zopfli
+
+# Don't install Node.js through Homebrew. Use nvm (https://www.taniarascia.com/setting-up-a-brand-new-mac-for-development/#nodejs).
+
+# Install binaries
+brew cask install colorsnapper
+brew cask install firefox
+brew cask install google-chrome
+# brew cask install google-photos-backup-and-sync
+# brew cask install figma
+brew cask install microsoft-word
+brew cask install microsoft-excel
+# brew cask install microsoft-powerpoint
+# brew cask install numi
+brew cask install open-in-code
+# brew cask install postman
+brew cask install private-internet-access
+brew cask install rectangle
+brew cask install virtualbox
+brew cask install visual-studio-code
+brew cask install vlc
+brew cask install whatsapp
+
+# Install manually
+# uTorrent, ulysses, rocket, pdfExpert, mindNode, microsoft remoteDesktop, chrome canary, 
 
 # Remove outdated versions from the cellar.
 brew cleanup
